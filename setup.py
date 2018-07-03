ffebce2391300080785c3d5c888ddae064caba0e
Xavier SCHMITT <xavier.schmitt@nist.gov>
2018-07-03 08:45:55 -0400

    chore: update beta4
    Change-Id: I1b25d2c91da2bae8007869e041b3c46f79245303

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_module_remote_blob_host_app",
-    version="1.0.0-beta3",
+    version="1.0.0-beta4",
     description="Remote Blob Host module for the parser core project",
     long_description=long_desc,
     author="NIST IT Lab",
